15904d3981f6d59225dd7768359bdb9ed8208152
abcb-s <smileehd03@dgsw.hs.kr>
2025-04-21 20:00:01 +0900

    [Bronze III] Title: 최댓값, Time: 176 ms, Memory: 17620 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] a = new int[9];
+        for (int i = 0; i < a.length; i++) {
+            a[i] = sc.nextInt();
+        }
+        int max = a[0];
+        int maxIndex = 1;
+        for (int i = 1; i < a.length; i++) {
+            if (a[i] > max) {
+                max = a[i];
+                maxIndex = i+1;
+            }
+
+        }
+        System.out.println(max);
+        System.out.println(maxIndex);
+    }
+}
