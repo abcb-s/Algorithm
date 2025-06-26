d6dfc33c25d78be2c3ec73e74cc3fd0f040eecb2
김준현 <smileehd03@dgsw.hs.kr>
2025-06-26 21:36:38 +0900

    [Bronze IV] Title: !밀비 급일, Time: 176 ms, Memory: 17376 KB -BaekjoonHub

---
+++ 백준/Bronze/11365. ！밀비 급일/！밀비 급일.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while(true) {
+            String s = sc.nextLine();
+            if(s.equals("END")) {
+                
+                break;
+            }
+            s = new StringBuilder(s).reverse().toString();
+            System.out.println(s);
+        }
+    }
+}
